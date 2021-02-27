a78b3b3c51c1fc711f0a5bf3398ff56353b8984e
lucasponez <lucasponez@users.noreply.github.com>
2021-02-27 13:52:00 +0000

    sanders: Back fingerprint to walleye (Pixel2) 8.1
    To fix cts Profile: false

--- ancient_sanders.mk
+++ ancient_sanders.mk
@@ -48,8 +48,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sanders-user 8.1.0 OPS28.65-36 9fea release-keys"
 
 # FINGERPRINT
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
-VENDOR_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+VENDOR_BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
 
 # for specific
 $(call inherit-product, vendor/motorola/sanders/sanders-vendor.mk)
